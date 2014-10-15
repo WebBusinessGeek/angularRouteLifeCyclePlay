a80254331a179f3d9ad9e1eed20194a6f94fefd3
Kevin Washington <kevw12188@gmail.com>
2014-10-14 20:50:40 -0400

    example workthrough

--- app.js
+++ app.js
@@ -1,21 +1,90 @@
 var app = angular.module('app',['ngRoute']);
 
 
-app.config(function($routeProvider){
+//configure routes and catchall handling with otherwise
+app.config(function($routeProvider, $locationProvider){
 	$routeProvider
-		.when('/hey', 
-		{
-			template: 'This is a template',
-			controller: 'someController'
+		.when('/',{
+			template: 'this is the home page',
+			controller:'routeController'
+		})
+		.when('/regular',{
+			template: 'this is the regular route',
+			controller: 'routeController'
+		})
+		.when('/delayed',{
+			template: 'this is the delayed route',
+			controller: 'routeController'
+		})
+		.when('/error', {
+			template: 'this is the error route',
+			controller: 'routeController'
 		})
 		.otherwise({
-			redirectTo: '/hey',
+			redirectTo: '/'
 		});
 })
 
 
+//check if route is changing and alerts when change is complete
+// app.controller('routeController', function($scope, $rootScope){
+// 	$rootScope.$on('$routeChangeStart', function(){
+// 		console.log('the route is starting to change');
+// 	})
+// 	$rootScope.$on('$routeChangeSuccess', function(){
+// 		console.log('the route is done changing');
+// 	})
+// })
 
 
-app.controller('someController', function($scope){
-	$scope.stuff = 'Some stuff';
+app.directive('routeDirective', function(){
+	return{
+		restrict: 'A',
+		controller: function($scope, $location, $rootScope){
+			$rootScope.$on('$routeChangeStart', function(){
+				console.log('the route is starting to change');
+			});
+			$rootScope.$on('$routeChangeSuccess', function(){
+				console.log('the route is done changing');
+			});
+		},
+		controllerAs: 'routeController'
+	}
+})
+
+
+app.directive('someController', function(){
+	return{
+		controller: function($scope, $location, $rootScope){
+			$scope.check = '';
+
+			$scope.newView = function(arg){
+				$location.path(arg);
+				$scope.check = arg;
+			}
+
+			$scope.check2 = 'check2';
+
+		}
+	}
+});
+
+app.directive('regularRoute', function(){
+	return{
+		restrict: 'E',
+		template: '<div class="col-md-4">'+
+            '<button class="btn btn-success" ng-click="newView(\'/regular\')">'+
+            'regular route and echo start/finish text below </button>{{check}}'+
+        	'</div>',
+       require: '^someController',
+		link: function(scope, element, attrs, someController){
+			// element.bind('click',function(){
+			// 	if(scope.check == '/regular'){
+			// 		console.log('its equal');
+			// 	}else{
+			// 		consloe.log('not equal');
+			// 	}
+			// });
+		}
+	}
 })
